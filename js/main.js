0a54ed93c788487bd2209b5b7d2966cee9f834b9
mavstronaut <mavbarona@gmail.com>
2019-06-18 20:40:35 -0400

    implementing javascript for game

--- js/main.js
+++ js/main.js
@@ -1,202 +1,125 @@
-var word = "";
-var wordGuess = [];
-var wrongGuess = [];
-var guessBomb = 0;
-var winCount = 1;
-var guess = "";
-var dif = 0;
+$(document).ready(function() {
 
-function chooseDif1() {
-    dif = 1;
-    document.getElementById('startButton').style.display='block';
-    document.getElementById('chooseDifficulty').style.display='none';
-}
-
-function chooseDif2() {
-    dif = 2;
-    document.getElementById('startButton').style.display='block';
-    document.getElementById('chooseDifficulty').style.display='none';
-}
-
-function chooseDif3() {
-    dif = 3;
-    document.getElementById('startButton').style.display='block';
-    document.getElementById('chooseDifficulty').style.display='none';
-}
+    
+        var plumbusvalue = 0;
+        var defensevalue = 0;
+        var speedvalue = 0;
+        var megavalue = 0;
+        var goalnumber = 0;
+        var wins = 0;
+        var losses = 0;
+        var crystaltotal = 0;
 
 
-function wordw() {
-    var randomWords = ["Waymar", "Gared", "Will", "Mycah", "Lady", "Wallen", "Stiv", "Vardis", "Wyl", "Heward", "Willis", "Kurleket", "Viserys", "Targaryen", "Mago", "Qotho", "KhalDrogo", "Drogo", "MirriMazDurr", "Robert", "Baratheon", "Varly", "VayonPoole", "SeptaMordane", "JoryCassel", "SyrioForel", "EddardStark", "Stark", "Barra", "Cressen", "Rakharo", "TheSpiceKing", "Irri", "PyatPree", "Doreah", "XaroXhoan", "Daxos", "Lommy", "Yoren", "Tickler", "AmoryLorch", "RenlyBaratheon", "AltonLannister", "Lannister", "TorrhenKarstark", "Torrhen", "Tom", "Maege", "Mandon", "Mormont", "Moore", "MathhosSeaworth", "Drennen", "Rennick", "TheHighSepton", "QhorinHalfhand", "RodrikCassel", "Lewin", "Craster", "JeorMormont", "Jeor", "Skinner", "WillemLannister", "Martyn", "Rickard", "Ros", "Kraznys", "Greizhen", "Prendahl", "Mero", "Robb", "GreyWind", "Wendel", "Joffrey", "Polliver", "Lowell", "Karl", "Rast", "Styr", "Locke", "Rorge", "Biter", "Kegs", "Mully", "JackBulwer", "Guymon", "TheLordofBones", "MagMar", "Grenn", "Dongo", "Smitty", "Cooper", "Donnel", "Pypar", "Oznak", "Mossador", "Ygritte", "LysaArryn", "Ralf", "Oberyn", "Martell", "Shae", "Tywin", "WhiteRat", "Myranda", "Meryn", "Janos", "Loboda", "Mance", "Gordy", "Summer", "Balon", "LadyCrane", "Shaggydog"]
-    var raNum = Math.floor(Math.random() * 114);
-    return randomWords[raNum].toLowerCase();
-    }
+        //game start - display wins and losses
 
+        // assign random value to each stone and generate the goal score
+        function randomizecrystalvalue (){
+            plumbusvalue = Math.floor(Math.random() * 12) + 1;
+            defensevalue = Math.floor(Math.random() * 12) + 1;
+            speedvalue = Math.floor(Math.random() * 12) + 1;
+            megavalue = Math.floor(Math.random() * 12) + 1;
+            console.log("Values in order: plumbus, defense, speed, mega");
+            console.log(plumbusvalue, defensevalue, speedvalue, megavalue);
+        };
 
-function wordStart() {
-    var wordLength = word.length;
-    var wordL_ = "";
-    var count = wordLength;
-
-    while(count > 0) {
-        wordGuess.push(" _ ");
-        count -= 1;
-    }
-}
-
-function winCountFunc() {
-    var num = 0;
-    var lettUsed = "";
-    var count = word.length;
-
-    while(count > 0) {
-        if(lettUsed.includes(word[count - 1])) {
-
+        function randomizegoalnumber () {
+            goalnumber = Math.floor(Math.random() * 102) + 19;
+            console.log("Goal number is...")
+            console.log(goalnumber)
+            $("#GoalNumber").text(goalnumber);
         }
 
-        else{
-            num += 1;
-            lettUsed += word[count - 1];
+        //show Total, wins, and losses
+
+        function showinitialvalues () {
+            $("#totalscore").text(crystaltotal);
+            $("#Wins").text(wins);
+            $("#Losses").text(losses);
+
+            
         }
 
-        count -= 1;
-    }
 
-    return num;
-}
+        gems = {
+            onclickFunction: function (clickValue){
+                if (clickValue==="plumbus"){
+                    clickValue= plumbusvalue
+                }else if(clickValue==="defense"){
+                    clickValue=defensevalue
+                }else if(clickValue==="speed"){
+                    clickValue=speedvalue
+                }else if(clickValue==="mega-seed"){
+                    clickValue=megavalue
+                }
 
-function start() {
-    word = wordw();
-    winCount = winCountFunc();
 
-    if(dif == 1) {
-        guessBomb = word.length + 5;
-    }
-
-    else if(dif == 2) {
-        guessBomb = word.length;
-    }
-
-    else if(dif == 3) {
-        if(word.length % 2 == 0) {
-           guessBomb = word.length / 2;
-        }
-      
-        else {
-           guessBomb = (word.length - 1) / 2;
-        }
-    }
-
-    console.log(word);
-    document.getElementById('mainGame').style.display='block';
-    document.getElementById('startButton').style.display='none';
-
-    document.getElementById("question").innerHTML = "Enter your first guess";
-
-    wordStart();
-
-    document.getElementById('RRguess').style.display='block';
-    document.getElementById("rightGuess").innerHTML = "word progress: " + wordGuess;
-    document.getElementById("wrongGuess").innerHTML = "Wrong guesses: " + wrongGuess;
-    document.getElementById("guessesLeft").innerHTML = "Guesses remaining: " + guessBomb;
-
-    var x = document.getElementById("guess").maxLength;
-    //document.getElementById("demo").innerHTML = x;
-}
-
-function enterGuess() {
-    var entry = "";
-    var lett = document.getElementById("guess").value.toLowerCase();
-    document.getElementById("guess").value = entry;
-
-    if (lett.length === 1){
-        var rightOnot = isRightOnot(lett);
-        if (rightOnot == true) {
-
-            NewCW(lett);
-        }
-
-        else {
-            if(!wrongGuess.includes(lett)) {
-                console.log("that's another sword in my butt");
-                wrongGuess.push(lett);
-            }
-            guessBomb -= 1;
-        }
-    }
-
-    else if (lett.length < 1) {
-
-    }
-
-    else {
-        guessBomb -= 1;
-    }
-
-    if (guessBomb <= 0) {
-        gameLose()
-    }
-
-    if (winCount <= 0) {
-        gameWin()
-    }
-    document.getElementById("rightGuess").innerHTML = "word progress: " + wordGuess;
-    document.getElementById("wrongGuess").innerHTML = "Wrong guesses: " + wrongGuess;
-    document.getElementById("guessesLeft").innerHTML = "Guesses remaining: " + guessBomb;
-}
-
-function isRightOnot(a) {
-    var n = word.includes(a);
-    return n;
-}
-
-function NewCW(letter) {
-    var count = 0;
-    winCount -= 1
-
-    while (count <= word.length - 1) {
-        if (letter === word[count]) {
-            if(wordGuess[count] === letter) {
-            }
-            else {
+                //====
+            
+                crystaltotal += clickValue;
+                console.log("Current Total = " + crystaltotal);
+                $("#totalscore").text(crystaltotal);
+                valuechecker();
             }
 
-            wordGuess[count] = letter;
-            count += 1;
         }
 
-        else {
-            count += 1;
+
+        // click commands to add value of each crystal to total and update screen with current crystal total
+        $(".buttons").on("click", function() {
+            idOfInput=$(this).attr("id")
+            gems.onclickFunction(idOfInput)
+        })
+
+
+        function valuechecker (){
+        if (crystaltotal===goalnumber) {
+
+            wins++;
+            $("#Wins").text(wins);
+            gamereset();
+        }
+        else if (crystaltotal>goalnumber) {
+            losses++;
+            $("#Losses").text(losses);
+            gamereset();
+        }
+        else {    
+        }
         }
 
-    }
+        function gamereset () {
+            randomizecrystalvalue();
+            randomizegoalnumber();
+            crystaltotal = 0;
+            showinitialvalues();
+        }
 
-}
+        gamereset ()
+        //if totalscore<goalnumber keep going
+        //else if totalscore = goalnumber win and increment wins count
+        //else if totalscore> goal number lose and increment losses count
 
-function gameLose() {
-    document.getElementById('mainGame').style.display='none';
-    document.getElementById('RRguess').style.display='none';
-    document.getElementById('youLose').style.display='block';
-    document.getElementById("correctWordWas").innerHTML = "The correct word was " + word;
-}
+        function gameLose() {
+            document.getElementById('mainGame').style.display='none';
+            document.getElementById('youLose').style.display='block';
+        }
 
-function gameWin() {
-    document.getElementById('mainGame').style.display='none';
-    document.getElementById('RRguess').style.display='none';
-    document.getElementById('youWin').style.display='block';
-}
+        function gameWin() {
+            document.getElementById('mainGame').style.display='none';
+            document.getElementById('RRguess').style.display='none';
+            document.getElementById('youWin').style.display='block';
+        }
 
-function restart() {
-    document.getElementById('mainGame').style.display='none';
-    document.getElementById('RRguess').style.display='none';
-    document.getElementById('youLose').style.display='none';
-    document.getElementById('youWin').style.display='none';
-    document.getElementById('chooseDifficulty').style.display='block';
+        function restart() {
+            document.getElementById('mainGame').style.display='none';
+            document.getElementById('RRguess').style.display='none';
+            document.getElementById('youLose').style.display='none';
+            document.getElementById('youWin').style.display='none';
+            document.getElementById('chooseDifficulty').style.display='block';
 
-    word = "";
-    wordGuess = [];
-    wrongGuess = [];
-    guessBomb = 0;
-    winCount = 1;
-    guess = "";
-    dif = 0;
-}
+
+        }
+
+        
+});
--- js/reset.js
+++ js/reset.js
@@ -3,46 +3,102 @@
    License: none (public domain)
 */
 
-html, body, div, span, applet, object, iframe,
-h1, h2, h3, h4, h5, h6, p, blockquote, pre,
-a, abbr, acronym, address, big, cite, code,
-del, dfn, em, img, ins, kbd, q, s, samp,
-small, strike, strong, sub, sup, tt, var,
-b, u, i, center,
-dl, dt, dd, ol, ul, li,
-fieldset, form, label, legend,
-table, caption, tbody, tfoot, thead, tr, th, td,
-article, aside, canvas, details, embed, 
-figure, figcaption, footer, header, hgroup, 
-menu, nav, output, ruby, section, summary,
-time, mark, audio, video {
-	margin: 0;
-	padding: 0;
-	border: 0;
-	font-size: 100%;
-	font: inherit;
-	vertical-align: baseline;
-}
+
 /* HTML5 display-role reset for older browsers */
-article, aside, details, figcaption, figure, 
-footer, header, hgroup, menu, nav, section {
-	display: block;
+// Version without passing functions
+
+var plumbusvalue = 0;
+var defensevalue = 0;
+var speedvalue = 0;
+var megavalue = 0;
+var goalnumber = 0;
+var wins = 0;
+var losses = 0;
+var crystaltotal = 0;
+
+//game start - display wins and losses
+
+// assign random value to each stone and generate the goal score
+function randomizecrystalvalue (){
+    plumbusvalue = Math.floor(Math.random() * 12) + 1;
+    defensevalue = Math.floor(Math.random() * 12) + 1;
+    speedvalue = Math.floor(Math.random() * 12) + 1;
+    megavalue = Math.floor(Math.random() * 12) + 1;
+    console.log("Values in order: Plumbus, Defense, Speed, Mega");
+    console.log(plumbusvalue, defensevalue, speedvalue, megavalue);
+};
+
+function randomizegoalnumber () {
+    goalnumber = Math.floor(Math.random() * 102) + 19;
+    console.log("Goal number is...")
+    console.log(goalnumber)
+    $("#GoalNumber").text(goalnumber);
 }
-body {
-	line-height: 1;
+
+//show Total, wins, and losses
+
+function showinitialvalues () {
+    $("#totalscore").text(crystaltotal);
+    $("#Wins").text(wins);
+    $("#Losses").text(losses);
+
+    
 }
-ol, ul {
-	list-style: none;
+
+// click commands to add value of each crystal to total and update screen with current crystal total
+$("#mega-seed").on("click", function() {
+    console.log(plumbusvalue);
+    crystaltotal += plumbusvalue;
+    console.log("Current Total = " + crystaltotal);
+    $("#totalscore").text(crystaltotal);
+    valuechecker();
+})
+
+$("#plumbus").on("click", function() {
+    console.log(defensevalue);
+    crystaltotal += defensevalue;
+    console.log("Current Total = " + crystaltotal);
+    $("#totalscore").text(crystaltotal);
+    valuechecker();
+})
+
+$("#defense").on("click", function() {
+    console.log(speedvalue);
+    crystaltotal += speedvalue;
+    console.log("Current Total = " + crystaltotal);
+    $("#totalscore").text(crystaltotal);
+    valuechecker();
+})
+
+$("#speed").on("click", function() {
+    console.log(megavalue);
+    crystaltotal += megavalue;
+    console.log("Current Total = " + crystaltotal);
+    $("#totalscore").text(crystaltotal);
+    valuechecker();
+})
+function valuechecker (){
+if (crystaltotal===goalnumber) {
+    wins++;
+    $("#Wins").text(wins);
+    gamereset();
 }
-blockquote, q {
-	quotes: none;
+else if (crystaltotal>goalnumber) {
+    losses++;
+    $("#Losses").text(losses);
+    gamereset();
 }
-blockquote:before, blockquote:after,
-q:before, q:after {
-	content: '';
-	content: none;
+else {}
 }
-table {
-	border-collapse: collapse;
-	border-spacing: 0;
+
+function gamereset () {
+    randomizecrystalvalue();
+    randomizegoalnumber();
+    crystaltotal = 0;
+    showinitialvalues();
 }
+
+gamereset ()
+//if totalscore<goalnumber keep going
+//else if totalscore = goalnumber win and increment wins count
+//else if totalscore> goal number lose and increment losses count
